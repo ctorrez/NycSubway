bb682953a4496b31112dd2dc1bf848c8324d2253
Christian Torrez <christian.torrez@hotmail.com>
2022-08-28 10:49:57 -0600

    Add project NycSubway.Infrastructur

---
+++ NycSubway.Infrastructure/Repositories/StationRepo.cs
@@ -0,0 +1,66 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using NycSubway.Core.Common;
+using NycSubway.Core.Services.Station;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NycSubway.Infrastructure.Repositories
+{
+    public class StationRepo : IStationRepo
+    {
+        public StationRepo()
+        {
+
+        }
+
+        public List<StationEntrance> GetStationEntrances()
+        {
+            var stations = new List<StationEntrance>();
+            var csvs = GetSubwayEntranceCsvs();
+
+            foreach(var x in csvs)
+            {
+                stations.Add(new StationEntrance()
+                {
+                    Name = x.Name,
+                    Point = GetGeoPoint(x.Geom)
+                });
+            }
+
+            return stations;
+        }
+
+        private GeoPoint GetGeoPoint(string geom)
+        {
+            // POINT (-73.86835600032798 40.84916900104506)
+
+            var values = geom.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            return new GeoPoint()
+            {
+                Latitude = double.Parse(values[1]),
+                Longitude = double.Parse(values[2])
+            };
+        }
+
+        private static List<SubwayEntranceCsv> GetSubwayEntranceCsvs()
+        {
+            var data = ResourceHelper.ReadResource("DOITT_SUBWAY_ENTRANCE_01_13SEPT2010.csv");
+
+            using (var reader = new StringReader(data))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<SubwayEntranceCsvMap>();
+
+                var records = csv.GetRecords<SubwayEntranceCsv>().ToList();
+
+                return records;
+            }
+        }
+    }
+}
---
+++ NycSubway.Infrastructure/Repositories/SubwayEntranceCsv.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper.Configuration;
+
+namespace NycSubway.Infrastructure.Repositories
+{
+    internal class SubwayEntranceCsv
+    {
+        public int ObjectId { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Geom { get; set; }
+        public string Line { get; set; }
+    }
+
+    internal class SubwayEntranceCsvMap : ClassMap<SubwayEntranceCsv>
+    {
+        public SubwayEntranceCsvMap()
+        {
+            // OBJECTID,URL,NAME,the_geom,LINE
+            Map(m => m.ObjectId).Name("OBJECTID");
+            Map(m => m.Url).Name("URL");
+            Map(m => m.Name).Name("NAME");
+            Map(m => m.Geom).Name("the_geom");
+            Map(m => m.Line).Name("LINE");
+        }
+    }
+}
