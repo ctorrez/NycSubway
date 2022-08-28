47908b03ebeeb9695eeb1975bcfc4f7b6928b310
Christian Torrez <christian.torrez@hotmail.com>
2022-08-28 12:09:10 -0600

    NycSubway.WebApi - add class ApplicationServicesExtensions to handle service injection

---
+++ NycSubway.WebApi/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NycSubway.Core.Services.Station;
+using NycSubway.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NycSubway.WebApi.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IStationRepo, StationRepo>();
+            services.AddScoped<StationService, StationService>();
+
+            return services;
+        }
+    }
+}
--- NycSubway.WebApi/Startup.cs
+++ NycSubway.WebApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NycSubway.WebApi.Extensions;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -45,6 +46,7 @@ namespace NycSubway.WebApi
             });
 
             services.AddControllers();
+            services.AddApplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
